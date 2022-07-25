7064baa13a88c1b123c6a4fd8088e0ac02348175
surech <stefan@surech.ch>
2022-07-25 18:16:01 +0200

    stu: externalize configuration

--- chronos-analyser/src/main/java/ch/surech/chronos/chronosanalyser/service/AuthentificationService.java
+++ chronos-analyser/src/main/java/ch/surech/chronos/chronosanalyser/service/AuthentificationService.java
@@ -8,6 +8,7 @@ import com.microsoft.graph.requests.GraphServiceClient;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,15 +16,36 @@ public class AuthentificationService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthentificationService.class);
 
+    @Value("${microsoft.graph.auth.clientId}")
+    private String clientId;
+
+    @Value("${microsoft.graph.auth.clientSecret}")
+    private String clientSecret;
+
+    @Value("${microsoft.graph.auth.authorityHost}")
+    private String authorityHost;
+
+    @Value("${microsoft.graph.auth.tenantId}")
+    private String tenantId;
+
+    @Value("${microsoft.graph.auth.scopes}")
+    private String[] appScopes;
+
     public void makeConnection(){
+        this.LOGGER.info("ClientID: {}", clientId);
+        this.LOGGER.info("Client Secret: {}", clientSecret);
+        this.LOGGER.info("Authority Host: {}", authorityHost);
+        this.LOGGER.info("Tenant ID: {}", tenantId);
+        this.LOGGER.info("Scopes: {}", appScopes);
+
         final ClientSecretCredential clientSecretCredential = new ClientSecretCredentialBuilder()
-            .clientId("xx")
-            .clientSecret("xx")
-            .authorityHost("xx")
-            .tenantId("xx")
+            .clientId(clientId)
+            .clientSecret(clientSecret)
+            .authorityHost(authorityHost)
+            .tenantId(tenantId)
             .build();
 
-        final TokenCredentialAuthProvider tokenCredentialAuthProvider = new TokenCredentialAuthProvider(List.of("xx"), clientSecretCredential);
+        final TokenCredentialAuthProvider tokenCredentialAuthProvider = new TokenCredentialAuthProvider(List.of(appScopes), clientSecretCredential);
 
         final GraphServiceClient graphClient =
             GraphServiceClient
